7996309344dfc956c4a3e475932ccbf6b6b23ab0
David-Ykz <kzdavid314@gmail.com>
2024-01-16 21:42:09 -0500

    initialized frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Helloworld from "./Helloworld";
+
+function App() {
+  return (
+    <div>
+      <Helloworld/>
+    </div>
+  );
+}
+
+export default App;
